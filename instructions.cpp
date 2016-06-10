ee6dbf345834cd9701dcb154599ada462b2a4dc6
SahanaRamnath <sahanjich@gmail.com>
2016-06-11 01:02:24 +0530

    Instructions to use the code

---
+++ instructions.cpp
@@ -0,0 +1,18 @@
+//defining function instructions()
+#include <iostream>
+
+using namespace std;
+
+void instructions()
+{
+     cout<<"INSTRUCTIONS : \n";
+     cout<<"This is a program on Hand-Gesture-Based-Control.\n";
+     cout<<"Once you finish reading the instructions, press Enter. You have to place your hand inside the red rectangle drawn on the screen. Once you do that, press the letter 's'.\n";
+
+     cout<<"1.Moving the hand to your left leads to a decrease in volume.\n";    
+     cout<<"2.Moving your hand to the right leads to an increase in volume.\n";
+
+     cout<<"As of now, moving your hand up and down do not lead to any specific system command. However, this feature can be enabled by uncommenting certain lines in the code (mentioned in the code itself).\n";
+
+     cout<<"Note : This program is subject to background conditions and may not give accurate results at all time.";
+}
